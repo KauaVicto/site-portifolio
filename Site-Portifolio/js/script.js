3a4a66f592f7387653e9a4224e7fd3a39e82288a
RebbD <kauavictor259@gmail.com>
2021-05-01 10:45:55 -0300

    Add as animações

--- Site-Portifolio/js/script.js
+++ Site-Portifolio/js/script.js
@@ -4,16 +4,18 @@ let mainHome = document.getElementById('home')
 let mainProjetos = document.getElementById('projetos')
 
 
-let tagHome = document.getElementById('tag-home')
-let tagProjetos = document.getElementById('tag-projetos')
+let tag1 = document.getElementById('tag-home')
+let tag2 = document.getElementById('tag-curiosidades')
+let tag3 = document.getElementById('tag-games')
+let tag4 = document.getElementById('tag-projetos')
 
-function PageProjetos(){
+function Page4(){
     mainHome.style.display = 'none'
     mainProjetos.style.display = 'block'
     document.body.style.height = '2050px'
 }
 
-function PageHome(){
+function Page1(){
     mainHome.style.display = 'block'
     mainProjetos.style.display = 'none'
     document.body.style.height = '1350px'
@@ -21,18 +23,40 @@ function PageHome(){
 }
 
 
-
-function MostrarHome(){
-    tagHome.style.display = 'block'
+//mostrar e tirar tag do home
+function Mostrar1(){
+    tag1.style.opacity = '1'
+    tag1.style.top = '0px'
 }
-function TirarHome(){
-    tagHome.style.display = 'none'
+function Tirar1(){
+    tag1.style.opacity = '0'
+    tag1.style.top = '-20px'
 }
-
-function MostrarProjetos(){
-    tagProjetos.style.display = 'block'
+//mostrar e tirar tag de curiosidades
+function Mostrar2(){
+    tag2.style.opacity = '1'
+    tag2.style.top = '0px'
 }
-function TirarProjetos(){
-    tagProjetos.style.display = 'none'
+function Tirar2(){
+    tag2.style.opacity = '0'
+    tag2.style.top = '-20px'
+}
+//mostrar e tirar tag de games
+function Mostrar3(){
+    tag3.style.opacity = '1'
+    tag3.style.top = '0px'
+}
+function Tirar3(){
+    tag3.style.opacity = '0'
+    tag3.style.top = '-20px'
+}
+//mostrar e tirar tag de projetos
+function Mostrar4(){
+    tag4.style.opacity = '1'
+    tag4.style.top = '0px'
+}
+function Tirar4(){
+    tag4.style.opacity = '0'
+    tag4.style.top = '-20px'
 }
 
